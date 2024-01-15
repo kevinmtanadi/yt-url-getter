46af44d71acdf55e69bb01b126c6ae50d43c2594
kevinmtanadi1 <kevin.tanadi@ruparupa.com>
2024-01-15 15:56:29 +0700

    first init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/get_url', async (req, res) => {
+  const videoURL = req.query.url;
+
+  if (!videoURL) {
+    return res.status(400).json({ error: 'Missing video URL' });
+  }
+
+  const info = await ytdl.getInfo(videoURL);
+  let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+
+  ytdl.downloadFromInfo(info, { filter: 'audioonly' }).pipe(res);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
